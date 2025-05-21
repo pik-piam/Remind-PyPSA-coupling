09486e1b1831744371890ae7fe100ef2ccfc7c08
irr-github <ivan.ramirez@pik-potsdam.de>
2025-05-21 15:55:34 +0200

    add validator

--- src/rpycpl/disagg.py
+++ src/rpycpl/disagg.py
@@ -10,6 +10,29 @@ import numpy as np
 
 class SpatialDisaggregator:
 
+    def __init__(self, targets=None):
+        self._target_nodes = targets
+
+    def validate_reference_data(self, reference_data: pd.Series):
+        """Check reference data has expected format
+        Args:
+            reference_data (pd.Series): The reference data for disaggregation.
+        Raises:
+            TypeError: If reference data is not a pandas Series.
+            ValueError: If reference data index does not match target nodes.
+            ValueError: If reference data is not normalised to 1.
+        """
+
+        if not isinstance(reference_data, pd.Series):
+            raise TypeError("Reference data must be a pandas Series")
+        if self._target_nodes:
+            if not reference_data.index.isin(self._target_nodes).all():
+                raise ValueError(
+                    f"Reference data index {reference_data.index} does not match target nodes {self._target_nodes}"
+                )
+        if not reference_data.sum() == 1:
+            raise ValueError("Reference data is not normalised to 1")
+
     def use_static_reference(self, data: pd.Series, reference_data: pd.Series):
         """
         Use a reference year to disaggregate the quantity spatially
@@ -20,10 +43,10 @@ class SpatialDisaggregator:
         Returns:
             pd.DataFrame: The disaggregated data. Dims: (space, year).
         """
-        if not reference_data.sum() == 1:
-            raise ValueError("Reference data is not normalised to 1")
 
+        self.validate_reference_data(reference_data)
         # outer/cartersian product to get (years, region) matrix
         return pd.DataFrame(
             np.outer(data, reference_data), index=data.index, columns=reference_data.index
         ).T
+    
