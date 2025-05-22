e34f685b8c59d662fdda24d5b5d6a17ac6dffc03
irr-github <ivan.ramirez@pik-potsdam.de>
2025-05-22 16:25:13 +0200

    add shell

---
+++ examples/multistep.py
@@ -0,0 +1,7 @@
+""" Advanced example illustrating a multi-step ETL process using the rpycpl package.
+
+In general consider using single steps and chaining them together with snakemake or another 
+workflow manager.
+"""
+
+raise NotImplementedError("This example is not implemented yet")
